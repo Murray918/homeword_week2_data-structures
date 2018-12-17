1dade19ce662d02adb99cc6be197c18a9ec0c64b
pittpir <pittpir@gmail.com>
2018-12-17 17:28:27 -0600

    Part #4 -- Object-ception

--- app.js
+++ app.js
@@ -105,6 +105,28 @@ console.log(crazyObject.larry.characters[1])
 
 console.log(`******* #4) Object-ception *******` );
 
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
 
+//change the value of limbo to null
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
+
+console.log(`******* #5) Bond Films *******` );
 
 
