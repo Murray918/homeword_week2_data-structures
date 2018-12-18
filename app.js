8a4f36c234f83eb0a80ae8849c98ab9c5d5370b9
AJ <ahumphries102@gmail.com>
2018-12-17 19:40:15 -0600

    data types and data structures

---
+++ app.js
@@ -0,0 +1,33 @@
+// Data-Types 1
+// A light switch that can be either on or off.
+// boolean
+
+// A user's email address.
+// string
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// object
+
+// A list of student names from our class.
+// Array
+
+// A list of student names from our class, each with a location.
+// an Array with objects inside
+
+/* A list of student names from our class, each with a location 
+and each with a list of favorite tv shows.*/
+// an array with an object and an array within
+
+// Take It Easy 2
+// 1 & 2
+// let colorRain = ["blue", "red", "yellow", "green", "orange", "indigo"]
+// colorRain[0]
+
+//3
+let yourName = {
+	favoriteFood: "pizza",
+	hobby: "magic",
+	homeTown: "atlantis",
+	favoriteData: "arrays"
+}
+yourName.hobby
