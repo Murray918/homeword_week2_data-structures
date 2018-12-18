4e42bc5cf32e5878994417077fce560935b3e298
ohheyitslance <ohheyitslance@ohheyitsMacBook.attlocal.net>
2018-12-17 22:09:06 -0600

    2 complete

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-// // 1. Data Types
-// A light switch that can be either on or off.
-let lightSwitch = false
-// A user's email address
-let usersEmail = "user@email.com"
-// A spaceship with a hull, laster blasters, tractorm beam, and warp drive.
-let spaceshipParts = ["hull", "laser blasters", "tractor beam", "warp drive"]
-// A list of student names from our class.
-let studentNames = ["Lance", "AJ", "Andrew", "Josh", "Christopher", "Jacob", "Jonathan", "Jamal", "Jules", "Tristan"]
-// A list of student names from our class, each with a location.
-let students = {
-	name:
-	location:
+// // // 1. Data Types
+// // A light switch that can be either on or off.
+// let lightSwitch = false
+// // A user's email address
+// let usersEmail = "user@email.com"
+// // A spaceship with a hull, laster blasters, tractorm beam, and warp drive.
+// let spaceshipParts = ["hull", "laser blasters", "tractor beam", "warp drive"]
+// // A list of student names from our class.
+// let studentNames = ["Lance", "AJ", "Andrew", "Josh", "Christopher", "Jacob", "Jonathan", "Jamal", "Jules", "Tristan"]
+// // A list of student names from our class, each with a location.
+// let students = {
+// 	name:
+// 	location:
+// }
+// // A list of student names from our class, each with location, and a list of favorite tv shows.
+// let allOfIt = {
+// 	[name:
+// 	location:
+// 	favTvShow:
+// 	]
+// }
+
+// 2.
+let rainbow = ["red", "orange", "blue", "green", "indigo", "violet"]
+console.log(rainbow[2])
+let Lance = {
+	favFood: "pho",
+	hobby: "cooking",
+	currentCity: "Austin",
+	favDataType: "arrays"
 }
-// A list of student names from our class, each with location, and a list of favorite tv shows.
-let allOfIt = {
-	[name:
-	location:
-	favTvShow:
-	]
-}
+console.log(Lance.hobby)
 
