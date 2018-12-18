785be7ada42a6be54d2e3e65956422921b371028
Andrew Aluko <aluko17@andrews-air.attlocal.net>
2018-12-17 18:36:47 -0600

    data structures

---
+++ app.js
@@ -0,0 +1,30 @@
+// Data Types
+
+/*
+boolean
+let lightSwitch 
+
+string
+let email = "aluko17@ymail.com"
+
+object
+spaceship = {
+	
+}
+object
+object
+object*/
+
+// Take it Easy
+
+let rainbow = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
+console.log(rainbow[2])
+let yourName = {
+	name: "Andrew",
+	favoriteFood: "Tacos",
+	hobby: "soccer",
+	location: "Austin",
+	favoriteDataType: "Boolean"
+}
+
+console.log(yourName.hobby)
