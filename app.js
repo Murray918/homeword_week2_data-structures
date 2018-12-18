8802ba0c7f4794e4ccc489835a197633ae0db93d
Jacob Werbin <jwerbin95@gmail.com>
2018-12-17 23:54:12 -0600

    crazy object

---
+++ app.js
@@ -0,0 +1,56 @@
+//boolean
+//string
+//object
+//array
+//array of objects
+//array of objects that all contain arrays
+let rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+console.log(rainbow[4])
+let Jacob = {
+	food: "All of it",
+	hobby: "PC Building",
+	town: "Lakeway",
+	favoriteDatatype: null
+}
+console.log(Jacob.hobby)
+
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
+}
+console.log(crazyObject.taco[1].salsa[5])
+console.log(crazyObject.larry.quotes[0])
+console.log(crazyObject.larry.characters[2].favourtieHobby)
+console.log(crazyObject.larry.nicknames[1])
+console.log(crazyObject.larry.characters[1])
+
+
+
+
