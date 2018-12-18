425145010ad64e02fb5ec89426f4108278d7d168
AJ <ahumphries102@gmail.com>
2018-12-17 20:07:09 -0600

    object-ception

--- app.js
+++ app.js
@@ -33,39 +33,59 @@ and each with a list of favorite tv shows.*/
 // yourName.hobby
 
 // Crazy Object! 3
-const crazyObject = {
-  taco: [
-    {
-      meat: 'steak',
-      cheese: ['panela', 'queso', 'chihuahua']
-    },
-    {
-      meat: 'chicken',
-      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-    },
-  ],
-  larry: {
-    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-    characters: [
-      {
-        name: "Jeff",
-        occupation: "manager"
-      },
-      {
-        name: "funkhauser",
-        occupation: "tv dude"
-      },
-      {
-        name: "susie",
-        occupation: "jeffs wife",
-        favourtieHobby: "Swearing at Larry and Jeff"
-      },
-    ]
-  }
+// const crazyObject = {
+//   taco: [
+//     {
+//       meat: 'steak',
+//       cheese: ['panela', 'queso', 'chihuahua']
+//     },
+//     {
+//       meat: 'chicken',
+//       salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+//     },
+//   ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [
+//       {
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//     ]
+//   }
+// }
+// console.log(crazyObject.taco[1].salsa[5])
+// console.log(crazyObject.larry.quotes[0])
+// console.log(crazyObject.larry.characters[2].favourtieHobby)
+// console.log(crazyObject.larry.nicknames[1])
+// console.log("The object contains the name " + crazyObject.larry.characters[1].name)
+
+// Object-ception
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
 }
-console.log(crazyObject.taco[1].salsa[5])
-console.log(crazyObject.larry.quotes[0])
-console.log(crazyObject.larry.characters[2].favourtieHobby)
-console.log(crazyObject.larry.nicknames[1])
-console.log("The object contains the name " + crazyObject.larry.characters[1].name)
+inception.limbo = null
