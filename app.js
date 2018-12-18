a212ac5da93b8b1760e74271294f6d2e67fec471
ohheyitslance <ohheyitslance@ohheyitsMacBook.attlocal.net>
2018-12-17 21:56:56 -0600

    1 complete

---
+++ app.js
@@ -0,0 +1,22 @@
+// // 1. Data Types
+// A light switch that can be either on or off.
+let lightSwitch = false
+// A user's email address
+let usersEmail = "user@email.com"
+// A spaceship with a hull, laster blasters, tractorm beam, and warp drive.
+let spaceshipParts = ["hull", "laser blasters", "tractor beam", "warp drive"]
+// A list of student names from our class.
+let studentNames = ["Lance", "AJ", "Andrew", "Josh", "Christopher", "Jacob", "Jonathan", "Jamal", "Jules", "Tristan"]
+// A list of student names from our class, each with a location.
+let students = {
+	name:
+	location:
+}
+// A list of student names from our class, each with location, and a list of favorite tv shows.
+let allOfIt = {
+	[name:
+	location:
+	favTvShow:
+	]
+}
+
