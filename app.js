141945c78d457b18b9316b6238e1c05d30efbf20
Johnathan <reddjohnathan@gmail.com>
2018-12-18 08:43:22 -0600

    bond-films-gross

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ const inception = {
 }
 inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null
 console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
-*/
+
 
 const bondFilms = [
   { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
@@ -156,14 +156,14 @@ const bondFilms = [
   { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
   { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
 ];
-/*
+
 let bondTitles = []
 
 for(let name of bondFilms){
 	bondTitles.push(name.title)
     console.log(name.title)
 }
-*/
+
 
 let oddBonds = []
 
@@ -173,9 +173,9 @@ for(let i = 0; i < bondFilms.length; i++){
 	}
 }
 console.log(oddBonds)
+*/
 
-
-
+7.  Did not get this one done. Ran out of time.
 
 
 
